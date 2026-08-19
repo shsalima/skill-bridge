72483d418b0641da6730e51328965c9ec4afe84b
salima <sahisalima77@gmail.com>
2026-08-19 15:20:46 +0200

    configuration de la connexion à MongoDB

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
 
 export const connectDB = async () => {
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
-
-const PORT = process.env.PORT || 5000;
+// import "dotenv/config";
+// console.log("MONGO_URL :", process.env.MONGO_URL);
 
 connectDB();
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
