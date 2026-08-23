c7a0470fbe3e2ae421240b192d3143dcef2377ba
salima <sahisalima77@gmail.com>
2026-08-23 23:31:36 +0200

    validation des champs pour regiter avec express-validator

---
+++ server/src/validators/authValidator.js
@@ -0,0 +1,35 @@
+import { body } from "express-validator";
+
+
+export const registerValidator=[
+    body("nom")
+        .notEmpty()
+        .withMessage("Le nom est obligatoire"),
+    body("prenom")
+        .notEmpty()
+        .withMessage("Le prénom est obligatoire"),
+    body("email")
+        .notEmpty()
+        .withMessage("L'adresse e-mail est obligatoire")
+        .isEmail()
+        .withMessage("L'adresse e-mail est invalide"),
+    body("motDePasse")
+        .notEmpty()
+        .withMessage("Le mot de passe est obligatoire")
+        .isLength({min:6})
+        .withMessage("Le mot de passe doit contenir au moins 6 caractères"),
+    body("telephone")
+        .optional()
+        .isMobilePhone()
+        .withMessage("Le numéro de téléphone est invalide"),
+    body("role")
+        .optional()
+        .isIn(["Candidat", "Entreprise", "Administrateur"])
+        .withMessage("Le rôle est invalide"),
+    body("dateNaissance")
+        .optional()
+        .isISO8601()
+        .withMessage("La date de naissance est invalide"),
+
+
+]
