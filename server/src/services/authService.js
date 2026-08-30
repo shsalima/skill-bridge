b5934d23bcebc54239bffc5a9036e56584c47d76
salima <sahisalima77@gmail.com>
2026-08-30 18:04:59 +0200

    ajout service d'incription

---
+++ server/src/services/authService.js
@@ -0,0 +1,26 @@
+import User from "../models/User"
+import bcrypt from "bcryptjs"
+
+
+export const registerUser= async(userData)=>{
+    const {nom, prenom, email, motDePasse,telephone, photo, role, dateNaissance }=userData
+     const utilisateurExiste= await User.findOne({email})
+
+     if(utilisateurExiste){
+        throw new Error("cette adresse mail est déjà utilisé")
+
+     }
+
+     const motDePasseChifre= await bcrypt.hash(motDePasse,10)
+      const utilisateur= await User.create({
+        nom,
+        prenom,
+        email,
+        motDePasse:motDePasseChifre,
+        telephone,
+        photo,
+        role,
+        dateNaissance
+      })
+      return utilisateur
+}
