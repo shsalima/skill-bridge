f0fa56fce24a5d3f8a5fde8f2d17dd3ea54ca522
salima <sahisalima77@gmail.com>
2026-08-30 18:37:04 +0200

    ajout controller  d'incription

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,20 @@
+import { registerUser } from "../services/authService"
+
+
+export const register = async(req,res)=>{
+    try{
+        const utilisateur= await registerUser(req.body)
+
+        return res.status(201).json({
+            success: true,
+            message:" utilisateur crée avec succés",
+            data :utilisateur
+        })
+    }catch(error){
+        return res.status(400).json({
+            success: false,
+            message : error.message
+
+        })
+    }
+}
