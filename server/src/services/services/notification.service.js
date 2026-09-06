8bf047477de415ab5ee71a8c340ae230e684e693
salima <sahisalima77@gmail.com>
2026-09-06 19:00:08 +0200

    crée service pour créenotification

---
+++ server/src/services/services/notification.service.js
@@ -0,0 +1,11 @@
+import Notification from "../../models/Notification"
+
+
+export const createNotificationService= async(destinataireId,titre,message,type="Candidature")=>{
+    return await Notification.create({
+        destinataire:destinataireId,
+        titre,
+        message,
+        type
+    })
+}
