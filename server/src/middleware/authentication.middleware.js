60eb84fe24c38ac2f86038c54bb07de89a57ac74
salima <sahisalima77@gmail.com>
2026-09-08 00:30:26 +0200

    ajout middleware pour checkAdminLimit

--- server/src/middleware/authentication.middleware.js
+++ server/src/middleware/authentication.middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import User from "../models/User.js"
 
     
 
@@ -24,6 +25,32 @@ export const authentificationCheck=(req,res,next)=>{
         return res.status(401).json({
             success:false,
             message:"token invalide ou expiré"
+        })
+    }
+}
+
+
+
+export const checkAdminLimit=async(req,res,next)=>{
+    try{
+        const {role} =req.body
+
+        if(role === "Administrateur"){
+            const existingAdmin =await User.findOne({role: "Administrateur"})
+
+            if(existingAdmin){
+                return res.status(403).json({
+                    success:false,
+                    message:"Impossible de créer un autre administrateur. Un administrateur existe déjà"
+                })
+            }
+        }
+        next()
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message :"Erreur lors de la vérification du rôle administrateur",
+            error:error.message
         })
     }
 }
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -2,12 +2,12 @@ import express from "express"
 import { getProfile, login, register, updateProfile } from "../controllers/user.controller.js"
 import { loginValidator, registerValidator } from "../validators/authValidator.js"
 import { validate } from "../middleware/validate.js"
-import { authentificationCheck } from "../middleware/authentication.middleware.js"
+import { authentificationCheck, checkAdminLimit } from "../middleware/authentication.middleware.js"
 import { preventRoleUpdate } from "../middleware/preventRoleUpdate.middleware.js"
 
 const router= express.Router()
 
-router.post("/register",registerValidator,validate,register)
+router.post("/register",registerValidator,validate,checkAdminLimit,register)
 router.post("/login",loginValidator,validate,login)
 
 router.get("/profile",authentificationCheck,getProfile)
