ae5be6163cbce52bf2bd8d63a4e37abadf1ac75b
salima <sahisalima77@gmail.com>
2026-09-09 14:22:52 +0200

    ajout toute les fonctionnaité logique services

---
+++ server/src/services/formation.service.js
@@ -0,0 +1,30 @@
+import Formation from "../models/Formation";
+
+
+export const createFormationService=async(candidatId,formationData)=>{
+   return await Formation.create({
+        candidat:candidatId,
+        ...formationData,  
+   })
+}
+
+export const getMyFormationsService=async(candidatId)=>{
+    return await Formation.find({candidat:candidatId}).sort({dateDebut:-1})
+}
+
+
+export const deleteFormationService=async(formationId,candidatId)=>{
+    const formation=await Formation.findById(formationId)
+
+    if(!formation){
+        throw new Error("Formation non trouvée")
+    }
+
+    if(formation.candidat.toString()!==candidatId){
+        throw new Error("Vous n'êtes pas autorisé à supprimer cette formation")
+    }
+
+    await formation.deleteOne()
+    return formation
+
+}
